47bb688b6c370e950962eab602f761f298880318
Yurii Derevenko <derevenkoyurec@gmail.com>
2020-08-24 12:40:26 -0700

    deployment fixes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -27,7 +27,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
       anchorScrolling: 'enabled',
       onSameUrlNavigation: 'reload',
       scrollPositionRestoration: 'enabled'
